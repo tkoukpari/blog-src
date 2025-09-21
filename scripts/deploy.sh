33ddbe8df3220494018c48cce21cd2e73bda8951
Tiam Koukpari <tiamkoukpari@Tiams-MacBook-Air.local>
2025-09-21 17:26:35 -0400

    .

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,12 @@ set -euo pipefail
 
 dune clean
 dune build @runtest @default
-rm -rf deploy/*
+
+rm -rf deploy/category/*
+rm -rf deploy/posts/*
+rm -rf deploy/static/*
+rm -rf deploy/tag/*
+
 cp -r _build/default/site/* deploy/
 rm -f deploy/dune*
 rm -f deploy/static/dune*
